f5c78a1e01f7b9dda4592cf3e091fde32b15e9fa
dilara153232@gmail.com <dilara153232@gmail.com>
2021-02-17 17:42:02 +0300

    WebAPI added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -18,6 +18,7 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        
         public IResult Add(Product product)
         {
             //business codes
@@ -34,7 +35,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 16)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
